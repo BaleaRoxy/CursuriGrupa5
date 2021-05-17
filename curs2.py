89829b2c59048199a6e4038372a1e653715d697c
unknown <roxybalea@yahoo.com>
2021-05-17 16:49:49 +0300

    jhj

---
+++ curs2.py
@@ -0,0 +1,25 @@
+# print('ana are merepythonpython curs2p curs2.py')
+# print("ana \"are\"mere")
+nr=1
+culoare = 'rosii'
+variabila = "ana are mere"
+variab='ana are \n{1} mere \n{0}'.format(nr, culoare)
+varia=f'ana are {nr} mere {culoare}'
+
+print(variab)
+C=0
+a=1
+b=2
+# c = a+b
+# c+=a
+# print(c)
+# print(f"rezultatul este {c}")
+if conditie e adevarata:
+    print("conditie")
+elif conditie2 e adevarata:
+    print(conditie2)
+elif conditie3 e adevarata:
+    print("conditie3")
+..
+else
+print()
